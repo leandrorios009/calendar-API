a0b2df5280c8647cd6e63b273ab41b47d0d7c24d
Leandro Rios <leandro.rios.009@gmail.com>
2022-09-05 21:08:25 -0300

    agrego modificacion para manejo de errores en end point registro

--- middlewares/field-validator.js
+++ middlewares/field-validator.js
@@ -5,12 +5,38 @@ const fieldValidator = ( req, res = response, next ) => {
 
      //Manejo de errores
      const errors = validationResult( req );
-     if( !errors.isEmpty() ) {
-         return res.status(400).json({
-             ok: false,
-             errors: errors.mapped()
-         });
-     }
+     //if( !errors.isEmpty() ) {
+        // return res.status(400).json({
+          //   ok: false,
+           //  errors: errors.mapped()
+         //});
+     //}
+
+     if ( !errors.isEmpty() ) {
+ 
+        const errorsArray = [];
+     
+        const errorsMapped = errors.mapped();
+     
+        for ( const property in errorsMapped ) {
+     
+          if ( property === 'email' || property === 'password' ) {
+     
+            errorsArray.push( errorsMapped[ property ].msg );
+     
+          }
+     
+        }
+     
+        const errorsString = errorsArray.join( ', ' );
+     
+        return res.status( 400 ).json( {
+          ok: false,
+          msg: errorsString
+        } );
+     
+      }
+     
 
     next();
 }
